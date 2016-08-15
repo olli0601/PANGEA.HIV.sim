bb972cb07c6de71da92aecf17258308cdbf2a4e5
olli0601 <oliver.ratmann@ic.ac.uk>
2016-08-15 09:31:07 +0100

    run mvr

--- R/hptn071.prog.R
+++ R/hptn071.prog.R
@@ -178,7 +178,7 @@ sim.regional.args<- function(			yr.start=1985, yr.end=2020, seed=42,
 ##--------------------------------------------------------------------------------------------------------
 pipeline.various<- function()
 {
-	if(1)	#submit various
+	if(0)	#submit various
 	{
 		cmd			<- cmd.various()
 		cmd			<- cmd.hpcwrapper(cmd, hpc.nproc= 1, hpc.q='pqeelab', hpc.walltime=71, hpc.mem="6000mb")
@@ -189,7 +189,7 @@ pipeline.various<- function()
 		quit("no")		
 	}	
 	#	split into individual distances and run MVR on HPC because MDS needs a ton of RAM
-	if(0)
+	if(1)
 	{
 		require(big.phylo)
 		
